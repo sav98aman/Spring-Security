6944c9a3dd6a5582796f526111b0bda64c91ec20
sav98aman <savverma98@gmail.com>
2023-01-31 00:38:11 +0530

    adding First Secruity to permit to all User User to access my service

--- SpringSecurity/src/main/java/com/app/ProjectSecurityConfig.java
+++ SpringSecurity/src/main/java/com/app/ProjectSecurityConfig.java
@@ -3,24 +3,18 @@ package com.app;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig {
 	@Bean
 	public SecurityFilterChain masaiSecurityConfig(HttpSecurity http) throws Exception {
-			
 			http.authorizeHttpRequests((auth)->auth
-				.requestMatchers("/masai/welcomeP").authenticated()
-				.requestMatchers("/masai/welcome").permitAll()
+				.anyRequest().permitAll()// give to permit all Without any Security or lock all user acces 
 			).httpBasic();
 			return http.build();
+			
 	  }
 
-	 @Bean
-	 public PasswordEncoder passwordEncoder() {
-	        return NoOpPasswordEncoder.getInstance(); //Not recommended
-		}
+	 
 }
